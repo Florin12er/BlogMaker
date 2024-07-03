bfb2e7dc95ff4dad75a0385f9a0165341cc0d87a
florin <apetreiflorin236@gmail.com>
2024-07-03 19:17:10 +0200

    added jwt-decode module

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,14 +1,17 @@
-import { Navigate } from "react-router-dom";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import {jwtDecode} from "jwt-decode";
 
 const RequireAuth = ({ children }) => {
+  const navigate = useNavigate();
+
   useEffect(() => {
     const checkTokenValidity = () => {
       const token = localStorage.getItem("token");
       if (!token) {
         // No token found, redirect to login
-        return <Navigate to="/login" replace />;
+        navigate("/login");
+        return;
       }
 
       try {
@@ -16,18 +19,20 @@ const RequireAuth = ({ children }) => {
         if (decodedToken.exp * 1000 < Date.now()) {
           // Token expired, remove from localStorage and redirect to login
           localStorage.removeItem("token");
-          return <Navigate to="/login" replace />;
+          navigate("/login");
+          return;
         }
       } catch (error) {
         console.error("Error decoding token:", error);
         // Handle any decoding errors (e.g., invalid token format)
         localStorage.removeItem("token");
-        return <Navigate to="/login" replace />;
+        navigate("/login");
+        return;
       }
     };
 
     checkTokenValidity();
-  }, []);
+  }, [navigate]);
 
   return children;
 };
